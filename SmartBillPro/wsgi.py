3705fdb0de0fb7f744d1a9d63219e2d692e64721
Mohan Kumar T <mmohan8617@gmail.com>
2026-03-31 22:43:58 +0530

    Delete SmartBillPro directory

--- SmartBillPro/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""
-wsgi.py — Entry point for Google App Engine
-Gunicorn calls this file to start the app.
-"""
-from app import app, socketio
-
-# GAE calls this
-if __name__ == '__main__':
-    socketio.run(app)
